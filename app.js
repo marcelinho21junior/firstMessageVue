63a71a03ea35978699978885958d353b291b9b4c
marcelinho21junior <marcelo.belitardo@fatec.sp.gov.br>
2022-05-21 12:40:11 -0300

    commit started

---
+++ app.js
@@ -0,0 +1,15 @@
+const app = new Vue({
+    el:"#app",
+    data:{
+        message:"Hello Marcelito 21",
+        nome:"Marcelo",
+        idade:27
+    }
+})
+
+const work = new Vue({
+    el:"#work",
+    data:{
+        trabalho:"programador"
+    }
+})
